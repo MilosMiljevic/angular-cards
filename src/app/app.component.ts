07068f8e6bcadadcc6ed165b5272af369ce086fb
Milos <m.miljevic@hotmail.com>
2023-05-10 16:23:44 +0200

    cards project for practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      userName: 'nature',
+      content: 'I saw this neat tree today',
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      userName: 'mountain-lover',
+      content: 'That was a big mountain',
+    },
+    {
+      title: 'Biking',
+      imageUrl: 'assets/biking.jpeg',
+      userName: 'biker123',
+      content: 'I some biking today',
+    },
+  ];
 }
